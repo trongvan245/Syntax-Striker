d96783b33f92daff49577ed411c1131c9c67d7bc
Bui Trong Van <trongvan159357@gmail.com>
2024-05-22 15:18:43 +0700

    remove register header

--- src/layout/RegisterLayout/RegisterLayout.jsx
+++ src/layout/RegisterLayout/RegisterLayout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom'
-import RegisterHeader from '../../components/RegisterHeader/RegisterHeader'
+// import RegisterHeader from '../../components/RegisterHeader/RegisterHeader'
 import Footer from '../../components/Footer/Footer'
 
 export default function RegisterLayout({ children }) {
   return (
     <div>
-      <RegisterHeader />
+      {/* <RegisterHeader /> */}
       {children}
       <Outlet />
       <Footer />
