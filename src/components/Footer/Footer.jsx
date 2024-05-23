3a1f2170045fb98bdc066f319791eb42ea9e3926
Hong Phuc Le <lhplhp10102004@gmail.com>
2024-05-23 10:23:02 +0700

    Update responsive for header footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,11 +8,33 @@ function ContactInfo(props) {
   )
 }
 
+// function Card(props) {
+//   return (
+//     <div className='card d-flex justify-content-center align-items-center' style={{ width: '18rem' }}>
+//       <img
+//         className='card-img-top'
+//         src={props.image ? props.image : 'https://i.pinimg.com/originals/65/c1/07/65c107dc3add2c77d047bd940c201698.jpg'}
+//         alt='Card image cap'
+//         style={{
+//           maxWidth: '200px'
+//         }}
+//       />
+//       <div className='card-body text-center'>
+//         <h5 className='card-title'>{props.title}</h5>
+//         <p className='card-text text-center'>{props.text}</p>
+//         <a href={props.link} target='_blank' className='btn btn-primary'>
+//           Detail
+//         </a>
+//       </div>
+//     </div>
+//   )
+// }
+
 export default function Footer() {
   return (
     <>
       <footer className='bg-secondary-3 text-white text-center pb-3'>
-        <div className='bg-primary-dark d-flex justify-content-around p-3'>
+        <div className='bg-primary-dark d-flex justify-content-around p-3 overflow-scroll'>
           <ContactInfo name='Thái Nguyễn Gia Bảo' email='another.email@example.com' phone='+^_^' />
           <ContactInfo name='Lê Hồng Phúc' email='phuc.lechieuvinh@hcmut.edu.vn' phone='+84 962 534 955' />
           <ContactInfo name='Nguyễn Ngọc Minh Quốc' email='???@???.com' phone='@_@' />
