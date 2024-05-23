77e054643db81d46f92b50e34e614fe145dcf620
Hong Phuc Le <lhplhp10102004@gmail.com>
2024-05-23 19:17:44 +0700

    Change minor bug

--- src/components/LandingPage/Menu/LandingPageSampleMenu2.jsx
+++ src/components/LandingPage/Menu/LandingPageSampleMenu2.jsx
@@ -7,7 +7,7 @@ function SampleMenu2() {
       <h1 className={styles.headingMenu}>Restaurant Menu</h1>
       <div className={styles.menuItems}>
         {menuData.map((item) => (
-          <div key={item.id} className='menu-item'>
+          <div key={item.id} className={styles.menuItem}>
             <img src={item.image} alt={item.name} className={styles.menuItemImage} />
             <div className={styles.menuItemInfo}>
               <h3 style={{ fontWeight: 'bold' }}>{item.name}</h3>
