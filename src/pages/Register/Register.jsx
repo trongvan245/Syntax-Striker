adb2c9580a7937d4c832d53598111601cb3d3a68
Nguyen Truyen Tuan <tuan160404@gmail.com>
2024-05-25 09:49:51 +0700

    Update responsive for Register Final

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -121,48 +121,42 @@ const Register = () => {
       <form onSubmit={handleSubmit}>
         <div className='form-section'>
           <img className='form-section-RestaurantImage' src='src\assets\Images\Register\restaurant.webp' alt='' />
-          <h2 style={{ fontSize: '25px', fontWeight: 'bold', color: '#000000' }}>Register</h2>
+          <h2 style={{ fontSize: '25px', fontWeight: 'bold', color: '#000000' }}>Đăng ký tài khoản</h2>
           <input
             type='text'
             name='name'
-            placeholder='Restaurant name*'
+            placeholder='Tên nhà hàng*'
             value={form.name}
             onChange={handleChange}
             required
           />
-          <input
-            type='email'
-            name='email'
-            placeholder='Email address*'
-            value={form.email}
-            onChange={handleChange}
-            required
-          />
+          <input type='email' name='email' placeholder='Email*' value={form.email} onChange={handleChange} required />
           <input
             type='password'
             name='password'
-            placeholder='Password*'
+            placeholder='Mật khẩu*'
             value={form.password}
             onChange={handleChange}
             required
           />
           <p style={{ fontSize: '14px', color: 'red', marginLeft: '8px' }}>
-            Password must be at least 6 characters long and contain at least 1 lowercase letter, 1 uppercase letter and
-            1 number
+            Mật khẩu phải dài ít nhất 6 ký tự và chứa ít nhất 1 chữ cái thường, 1 chữ in hoa và 1 chữ số
           </p>
           <input
             type='password'
             name='confirm_password'
-            placeholder='Confirm password*'
+            placeholder='Xác nhận mật khẩu*'
             value={form.confirm_password}
             onChange={handleChange}
             required
           />
-          <h2 style={{ fontSize: '25px', fontWeight: 'bold', color: '#000000', marginTop: '20px' }}>Contact</h2>
+          <h2 style={{ fontSize: '25px', fontWeight: 'bold', color: '#000000', marginTop: '20px' }}>
+            Thông tin liên hệ cần thiết
+          </h2>
           <input
             type='text'
             name='owner_name'
-            placeholder='Owner fullname*'
+            placeholder='Tên chủ nhà hàng*'
             value={form.owner_name}
             onChange={handleChange}
             required
@@ -170,22 +164,23 @@ const Register = () => {
           <input
             type='tel'
             name='phone_number'
-            placeholder='Phone number*'
+            placeholder='Số điện thoại*'
             value={form.phone_number}
             onChange={handleChange}
             pattern='[0-9]*'
             required
           />
           <label>
-            <input type='checkbox' required /> {'   '} I accept the terms and privacy policy
+            <input type='checkbox' required style={{ marginTop: '18px' }} /> {'   '} Tôi đã đọc và chấp nhập các điều
+            khoản.
           </label>
-          <button type='submit' className='form-section-Button'>
-            Sign up
+          <button type='submit' className='form-section-Button' style={{ marginTop: '30px' }}>
+            Đăng ký
           </button>
           <p style={{ textAlign: 'center' }}>
-            Already have an account?{' '}
+            Bạn đã có tài khoản?{' '}
             <a href='/login' className='login-link'>
-              Log in
+              Đăng nhập
             </a>
           </p>
           {/* <div className='social-login'>
@@ -196,10 +191,15 @@ const Register = () => {
         </div>
 
         {window.innerWidth > 992 && (
-          <div className='form-section contact'>
+          <div className='form-section contact' style={{ display: 'flex', alignItems: 'center' }}>
             {/* <div className={`form-section contact ${showContact ? 'show' : ''}`}> */}
             {/* {!showContact ? ( */}
-            <img src='src\assets\Images\Register\cooker_masterchefVN.webp' alt='' className='contact-image' />
+            <img
+              src='src\assets\Images\Register\cooker_masterchefVN.webp'
+              alt=''
+              className='contact-image'
+              style={{ width: '90%' }}
+            />
             {/* // ) : ( */}
             {/* // <> */}
             {/* <h2 style={{ fontSize: '25px', fontWeight: 'bold' }}>Contact</h2>
